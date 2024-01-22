11d83ebe224449eb62f16ab8fbad5961eb8cedfa
HernanDPV41 <hernandpv41@gmail.com>
2024-01-21 19:18:06 -0500

    Add price.

---
+++ FirstActivity/CarDealer/CarDealer.Domain/Entities/Vehicles/Price.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Domain.Entities.Vehicles
+{
+    /// <summary>
+    /// Precio de una entidad del consecionario.
+    /// </summary>
+    public class Price
+    {
+        #region Properties
+
+
+        #endregion
+
+    }
+}
