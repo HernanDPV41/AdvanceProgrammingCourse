2253f573fdcd610e5531f00f55bcbdef7e14c317
HernanDPV41 <hernandpv41@gmail.com>
2024-02-11 16:32:54 -0500

    Add clients hierarchy.

---
+++ FirstActivity/CarDealer/CarDealer.Domain/Entities/Clients/EnterpriseClient.cs
@@ -0,0 +1,38 @@
+﻿using CarDealer.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Domain.Entities.Clients
+{
+    /// <summary>
+    /// Modela un cliente empresarial.
+    /// </summary>
+    public class EnterpriseClient : Client, IBrand
+    {
+        #region Properties
+
+        public string Brand { get; set; }
+
+        /// <summary>
+        /// Ubicación geográfica de la sede de la empresa cliente.
+        /// </summary>
+        public PhysicalLocation Location { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Inicializa un objeto <see cref="EnterpriseClient"/>.
+        /// </summary>
+        /// <param name="brand">Marca de la empresa.</param>
+        /// <param name="location">Ubicación geográfica de la empresa.</param>
+        public EnterpriseClient(string brand, PhysicalLocation location)
+        {
+            Brand = brand;
+            Location = location;
+        }
+
+    }
+}
