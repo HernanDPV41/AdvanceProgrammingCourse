4ed1b461f0242d8be3420ca01ec041ee8608b4b0
HernanDPV41 <hernandpv41@gmail.com>
2024-02-11 17:36:12 -0500

    Add orders and price converter.

---
+++ FirstActivity/CarDealer/CarDealer.Domain/Entities/Orders/BuyOrder.cs
@@ -0,0 +1,67 @@
+﻿using CarDealer.Domain.Entities.Clients;
+using CarDealer.Domain.Entities.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Domain.Entities.Orders
+{
+    public class BuyOrder
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// Cliente que realizó la compra.
+        /// </summary>
+        public Client Client { get;}
+
+        /// <summary>
+        /// Vehículo a comprar. 
+        /// </summary>
+        public Vehicle Vehicle { get; }
+
+        /// <summary>
+        /// Cantidad de unidades del vehículo compradas.
+        /// </summary>
+        public int Units { get; set; }
+
+        /// <summary>
+        /// Fecha de creación de la orden.
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+        
+        /// <summary>
+        /// Fecha en la que se ejecuta el pago.
+        /// </summary>
+        public DateTime? PaymentDay { get; set; }
+
+        /// <summary>
+        /// Indica si la orden de compra ya fue pagada;
+        /// </summary>
+        public bool IsPayed => PaymentDay is not null;
+
+        /// <summary>
+        /// Precio total a pagar por la orden.
+        /// </summary>
+        public Price TotalPrice => new Price(Vehicle.Price.Currency, Vehicle.Price.Value * Units);
+
+        #endregion
+
+        /// <summary>
+        /// Inicializa un objeto <see cref="BuyOrder"/>.
+        /// </summary>
+        /// <param name="client">Cliente que realiza la compra.</param>
+        /// <param name="vehicle">Vehículo que realiza la compra.</param>
+        /// <param name="units">Unidades del vehículo compradas.</param>
+        public BuyOrder(Client client, Vehicle vehicle, int units = 1)
+        {
+            Client = client;
+            Vehicle = vehicle;
+            Units = units;
+        }
+
+    }
+}
---
+++ FirstActivity/CarDealer/CarDealer.Domain/Utilities/Converters/PriceConverter.cs
@@ -0,0 +1,74 @@
+﻿using CarDealer.Domain.Entities.Types;
+using CarDealer.Domain.Entities.Vehicles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.Domain.Utilities.Converters
+{
+    /// <summary>
+    /// Herramienta para convertir precios de un tipo de moneda a otro.
+    /// </summary>
+    public class PriceConverter
+    {
+        /// <summary>
+        /// Valor actual de la moneda nacional.
+        /// </summary>
+        private const double MN_CURRENT_VALUE = 1;
+
+        /// <summary>
+        /// Valor actual del USD.
+        /// </summary>
+        private const double USD_CURRENT_VALUE = 120;
+
+        /// <summary>
+        /// Valor actual del Euro.
+        /// </summary>
+        private const double EURO_CURRENT_VALUE = 120;
+
+        /// <summary>
+        /// Valor actual del MLC.
+        /// </summary>
+        private const double MLC_CURRENT_VALUE = 120;
+
+        /// <summary>
+        /// Convierte un precio de una moneda a otra.
+        /// </summary>
+        /// <param name="currencyTarget">Moneda a la que se quiere convertir.</param>
+        /// <param name="price">Precio a convertir.</param>
+        /// <returns>Nuevo precio convertido.</returns>
+        public Price ConvertTo(MoneyType currencyTarget, Price price)
+        {
+            // Obteniendo el valor actual de la moneda a la que se quiere convertir.
+            double currentCurrencyTargetValue = GetCurrencyCurrentValue(currencyTarget);
+            // Obteniendo el valor actual de la moneda de la que se quiere convertir.
+            double oldCurrencyValue = GetCurrencyCurrentValue(price.Currency);
+
+            return new Price(currencyTarget, price.Value * (oldCurrencyValue / currentCurrencyTargetValue));
+        }
+
+        #region Helpers
+
+        /// <summary>
+        /// Obtiene el valor actual de cada tipo de moneda.
+        /// </summary>
+        /// <param name="type">Tipo de moneda.</param>
+        /// <returns>Valor actual de la moneda.</returns>
+        private double GetCurrencyCurrentValue(MoneyType type)
+        {
+            return type switch
+            {
+                MoneyType.MN => MN_CURRENT_VALUE,
+                MoneyType.USD => USD_CURRENT_VALUE,
+                MoneyType.MLC => MLC_CURRENT_VALUE,
+                MoneyType.Euro => EURO_CURRENT_VALUE,
+                _ => throw new NotImplementedException(),
+            };
+        }
+
+        #endregion
+
+    }
+}
