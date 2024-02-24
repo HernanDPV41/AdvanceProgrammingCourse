0ab053f1b970d1a02490f599ff477bc8fb35b116
HernanDPV41 <hernandpv41@gmail.com>
2024-02-24 18:16:11 -0500

    Add console project.

---
+++ SecondActivity/CarDealer/CarDealer.ConsoleApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using CarDealer.DataAccess.Concrete;
+using CarDealer.Domain.Entities.Common;
+using CarDealer.Domain.Entities.Types;
+using CarDealer.Domain.Entities.Vehicles;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealer.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Creando un contexto para interactuar con la Base de datos.
+            ApplicationContext appContext = new ApplicationContext("Data Source=CarDealerDB.sqlite");
+            // Verificando si la BD no existe
+            if(!appContext.Database.CanConnect())
+            {
+                // Migrando base de datos. Este paso genera la BD con las tablas configuradas en su migración.
+                appContext.Database.Migrate();
+            }
+
+            //Price carPrice = new Price(MoneyType.Euro, 4500);
+
+            //appContext.Set<Price>().Add(carPrice);
+            //appContext.SaveChanges();
+
+            //Car car = new Car("Peugeot", EnergySource.Petroleum, carPrice);
+
+            //appContext.Set<Car>().Add(car);
+            //appContext.SaveChanges();
+
+            var cars = appContext.Set<Car>().ToList();
+
+            foreach (var loadedCar in cars)
+            {
+                Price? price = appContext.Set<Price>().Find(loadedCar.PriceId);
+                if (price is null)
+                    throw new InvalidOperationException($"Cannot find price with Id {loadedCar.PriceId}.");
+                loadedCar.Price = price;
+            }
+
+        }
+    }
+}
