d289e08f15251b1e91ed8678ffceee84626a694e
HernanDPV41 <hernandpv41@gmail.com>
2024-03-26 00:15:06 -0400

    Add price service protos.

---
+++ FourthActivity/CarDealer/CarDealer.Services/Services/PriceService.cs
@@ -0,0 +1,25 @@
+﻿using CarDealer.DataAccess.Abstract.Common;
+using CarDealer.Domain.Entities.Types;
+using Grpc.Core;
+
+namespace CarDealer.Services.Services
+{
+    public class PriceService : Price.PriceBase
+    {
+
+        private IPriceRepository _priceRepository;
+
+        public PriceService(IPriceRepository repository)
+        {
+            _priceRepository = repository;
+        }
+
+        public override Task<PriceDTO> CreatePrice(CreatePriceRequest request, ServerCallContext context)
+        {
+            _priceRepository.BeginTransaction();
+            var price = _priceRepository.Create((MoneyType)request.MoneyType,request.Value);
+            return new Task<PriceDTO>(() => new PriceDTO() { Id = 2, MoneyType = MoneyTypes.Mn, Value = 2});
+        }
+
+    }
+}
