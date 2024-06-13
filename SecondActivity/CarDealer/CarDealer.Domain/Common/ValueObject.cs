5025634b08127bf19842d065747a86c8c43df798
HernanDPV41 <hernandpv41@gmail.com>
2024-06-13 09:48:09 -0400

    Simplify ValueObject implementation.

--- SecondActivity/CarDealer/CarDealer.Domain/Common/ValueObject.cs
+++ SecondActivity/CarDealer/CarDealer.Domain/Common/ValueObject.cs
@@ -11,20 +11,6 @@ namespace CarDealer.Domain.Common
     /// </summary>
     public abstract class ValueObject
     {
-        protected static bool EqualOperator(ValueObject left, ValueObject right)
-        {
-            if (left is null ^ right is null)
-            {
-                return false;
-            }
-            return ReferenceEquals(left, right) || left.Equals(right);
-        }
-
-        protected static bool NotEqualOperator(ValueObject left, ValueObject right)
-        {
-            return !(EqualOperator(left, right));
-        }
-
         protected abstract IEnumerable<object> GetEqualityComponents();
 
         public override bool Equals(object? obj)
@@ -39,21 +25,5 @@ namespace CarDealer.Domain.Common
             return this.GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
         }
 
-        public override int GetHashCode()
-        {
-            return GetEqualityComponents()
-                .Select(x => x != null ? x.GetHashCode() : 0)
-                .Aggregate((x, y) => x ^ y);
-        }
-
-        public static bool operator ==(ValueObject one, ValueObject two)
-        {
-            return EqualOperator(one, two);
-        }
-
-        public static bool operator !=(ValueObject one, ValueObject two)
-        {
-            return NotEqualOperator(one, two);
-        }
     }
 }
