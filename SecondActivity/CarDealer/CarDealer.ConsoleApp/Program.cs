4b846def26abe1bd2ba1e02aa620e1d143e545f2
HernanDPV41 <hernandpv41@gmail.com>
2024-06-12 23:04:08 -0400

    Add unit of work implementation.

--- SecondActivity/CarDealer/CarDealer.ConsoleApp/Program.cs
+++ SecondActivity/CarDealer/CarDealer.ConsoleApp/Program.cs
@@ -8,59 +8,11 @@ namespace CarDealer.ConsoleApp
     {
         static void Main(string[] args)
         {
+            // Definiendo string de conexión.
+            string connectionString = "Data Source = Data.sqlite";
 
-            //// Creando un contexto para interactuar con la Base de datos.
-            //ApplicationContext appContext = new ApplicationContext("Data Source=CarDealerDB.sqlite");
-            //// Verificando si la BD no existe
-            //if(!appContext.Database.CanConnect())
-            //{
-            //    // Migrando base de datos. Este paso genera la BD con las tablas configuradas en su migración.
-            //    appContext.Database.Migrate();
-            //}
-
-            //************ Create
-            //Price carPrice = new Price(MoneyType.Euro, 4500);
-
-            //appContext.Set<Price>().Add(carPrice);
-            //appContext.SaveChanges();
-
-            //Car car = new Car("Peugeot", EnergySource.Petroleum, carPrice);
-
-            //appContext.Set<Car>().Add(car);
-            //appContext.SaveChanges();
-
-            //*************** Get
-            //var cars = appContext.Set<Car>().ToList();
-
-            //foreach (var loadedCar in cars)
-            //{
-            //    Price? price = appContext.Set<Price>().Find(loadedCar.PriceId);
-            //    if (price is null)
-            //        throw new InvalidOperationException($"Cannot find price with Id {loadedCar.PriceId}.");
-            //    loadedCar.Price = price;
-            //}
-
-            //**************** Update
-            //var cars = appContext.Set<Car>().ToList();
-
-            //foreach (var loadedCar in cars)
-            //{
-            //    loadedCar.IsDescapotable = false;
-            //    appContext.Set<Car>().Update(loadedCar);
-            //}
-
-            //appContext.SaveChanges();
-
-            //***************** Delete
-            //var cars = appContext.Set<Car>().ToList();
-
-            //foreach (var loadedCar in cars)
-            //{
-            //    appContext.Set<Car>().Remove(loadedCar);
-            //}
-            //appContext.SaveChanges();
-
-
+            // Creando contexto a usar por repositorios.
+            
         }
 
     }
---
+++ SecondActivity/CarDealer/CarDealer.DataAccess/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using CarDealer.Contracts;
+using CarDealer.DataAccess.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DataAccess
+{
+    /// <summary>
+    /// Implementación de <see cref="IUnitOfWork"/>.
+    /// </summary>
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public Task SaveChangesAsync(CancellationToken token)
+        {
+            return _context.SaveChangesAsync(token);
+        }
+    }
+}
